f46f93b18d01c5308e7ee135c6f2eb6bf69e6f39
ali1k <hyperir@gmail.com>
2015-08-11 10:53:23 +0200

    added jsonld

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 var readJson = require('read-package-json');
+var jsonld = require('jsonld');
+
+
+var context = {
+  "name": "http://schema.org/name",
+  "homepage": {"@id": "http://schema.org/url", "@type": "@id"}
+};
+
+
 readJson('./package.json', console.error, false, function (er, data) {
   if (er) {
     console.error("There was an error reading the file")
     return
   }
-
-  console.error('the package data is', data)
+  var doc = {
+    "http://schema.org/name": data.name,
+    "http://schema.org/url": {"@id": data.homepage}
+  };
+  jsonld.compact(doc, context, function(err, compacted) {
+      console.log(JSON.stringify(compacted, null, 2));
+    });
 });
